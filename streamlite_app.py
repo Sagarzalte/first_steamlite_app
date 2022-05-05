60b1be44e69ae4b4bb21a4b52621cf61ebce07e4
Sagarzalte <104973115+Sagarzalte@users.noreply.github.com>
2022-05-05 14:34:29 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,4 +1,8 @@
 import streamlit
 
 streamlit.title('My Mom\'s New Healthy Dinner') 
+streamlit.header ('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, spinach & Rocket Smoothie')
+streamlite.text('Hard-Boiled Free-Range Egg')
 
