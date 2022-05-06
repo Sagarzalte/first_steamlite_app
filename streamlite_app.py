e7b7abb180f599aa3770ebbd45c3998298d5fd87
Sagarzalte <104973115+Sagarzalte@users.noreply.github.com>
2022-05-06 18:24:57 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -25,4 +25,4 @@ my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
-
+add_my_fruit= streamlit.multiselect("What fruit would yoy like to add?:", list(my_fruit_list.index)) 
