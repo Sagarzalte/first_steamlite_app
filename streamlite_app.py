efe7c297fc9cc1d1ed2c653d71571b1191972a61
Sagarzalte <104973115+Sagarzalte@users.noreply.github.com>
2022-05-09 12:50:07 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -50,7 +50,7 @@ except URLError as e:
     
 # Create a function
 def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + fruit_choice) 
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) 
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
