d8a573faf95deb58f2ae989f8919a68533e7f29d
Sagarzalte <104973115+Sagarzalte@users.noreply.github.com>
2022-05-09 12:29:42 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -56,15 +56,13 @@ def get_fruityvice_data(this_fruit_choice):
 
 # To display API response
 streamlit.header('Fruityvice Fruit Advice!')
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select fruit to get information")
-  else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-    steamlit.dataframe(back_from_function) 
- except:
-    pass
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+  streamlit.error("Please select fruit to get information")
+else:
+  back_from_function = get_fruityvice_data(fruit_choice)
+  steamlit.dataframe(back_from_function) 
     
 
 
